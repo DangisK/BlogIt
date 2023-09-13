356278b8cacf4ba101328da83fdf6129bfb5d677
Dangis <62296041+DangisK@users.noreply.github.com>
2023-09-13 18:23:27 +0300

    Delete front directory
    will keep front and back in separate repositories

--- front/src/components/comment/edit-comment-modal/index.js
+++
@@ -1,1 +0,0 @@
-export * from "./edit-comment-modal";
--- front/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./navbar";
-export * from "./rightbar";
-export * from "./sidebar";
-export * from "./feed";
-export * from "./navigation-bar";
